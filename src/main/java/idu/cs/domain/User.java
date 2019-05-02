684b17e565ef5c112b132c51af1f71d808128336
user <jiseongkimu@gmail.com>
2019-05-02 14:35:16 +0900

    boot-thymeleaf

---
+++ src/main/java/idu/cs/domain/User.java
@@ -0,0 +1,39 @@
+package idu.cs.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+   @Id
+   @GeneratedValue(strategy=GenerationType.AUTO)
+   private Long id;   
+   private String name;
+   private String company;
+   
+   public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getCompany() {
+	return company;
+}
+public void setCompany(String company) {
+	this.company = company;
+}
+
+public void setId(Long id) {
+	this.id = id;
+}
+public Long getId() {
+      return id;
+   }
+   
+   
+}
